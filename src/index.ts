5ffe8286b038c8edb1bdfdc7239ac772e15a67e9
Uriel Barbosa <uprieto@gmail.com>
2024-12-26 14:29:17 -0300

    feat: add a post route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import express from "express";
-import { log } from "node:console";
+import "dotenv/config";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+const port = process.env.PORT;
+
+const genAI = new GoogleGenerativeAI(String(process.env.API_KEY));
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(() => {
+app.post("/api/v1/chat", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    res.json({ response: text });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal Server Errror" });
+  }
+});
+
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
